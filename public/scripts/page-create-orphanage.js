a66e3b6b73209855d0e63d0950912411bbf862f5
Gabriel Salvatore <gabrielsaccoccio@hotmail.com>
2020-10-18 22:07:49 -0300

    nlw

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,82 @@
+// create map
+const map = L.map('mapid').setView([-27.2057496,-49.6582354], 15);
+
+
+// create and add titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68]
+})
+
+let marker;
+
+// create and add marker 
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    // remove icon 
+    marker && map.removeLayer(marker)
+
+    // add icon layer
+    marker = L.marker([lat, lng], { icon })
+    .addTo(map)
+})
+
+// add photo field
+function addPhotoField() {
+    // pegar o container de fotos #images
+    const container = document.querySelector('#images')
+    // pegar o container para duplicar .new-upload
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    // realizar o clone da última imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    // verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+
+    if(input.value == "") {
+        return
+    }
+
+    // limpar o campo antes de acionar ao container de imagens
+    input.value = ""
+
+    // adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length < 2) {
+        // limpar o valor do campo
+        return
+    }
+
+    // deletar o campo
+    span.parentNode.remove();
+}
+
+// select yes or no
+function toggleSelect(event) {
+    document.querySelectorAll('.button-select button')
+    .forEach((button) => button.classList.remove('active'))
+
+
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    const input = document.querySelector('[name="open_on_weekends"]')
+    input.value = button.dataset.value
+}
+
